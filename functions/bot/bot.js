c6aea0f8e4c29b65060fdfe24a9e745e5cd8a3eb
mehrad <mmfirst@protonmail.com>
2024-01-25 16:32:40 +0330

    modified:   functions/bot/bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -76,4 +76,15 @@ bot.on("inline_query", async (ctx) => {
     await ctx.answerInlineQuery(results);
 });
 
-bot.launch();
+exports.handler = async (event) => {
+    try {
+        await bot.handleUpdate(JSON.parse(event.body));
+        return { statusCode: 200, body: "" };
+    } catch (e) {
+        console.error("error in handler:", e);
+        return {
+            statusCode: 400,
+            body: "This endpoint is meant for bot and telegram communication",
+        };
+    }
+};

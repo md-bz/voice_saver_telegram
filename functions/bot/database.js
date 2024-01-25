ea15ba993417ef757343f0bc0528acf7fc8b467d
mehrad <mmfirst@protonmail.com>
2024-01-25 16:15:57 +0330

    first commit

---
+++ functions/bot/database.js
@@ -0,0 +1,52 @@
+const { createClient } = require("@supabase/supabase-js");
+
+const supabaseUrl = "https://tdltgflceomgyxobibit.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function insert(userId, voices, currentVoice = null) {
+    voices = JSON.stringify(voices);
+    const { data, error } = await supabase
+        .from("voice_messages")
+        .insert([{ userId, voices, currentVoice }])
+        .select();
+    return error;
+}
+
+async function read(userId) {
+    let { data: voice_messages, error } = await supabase
+        .from("voice_messages")
+        .select("voices,currentVoice")
+        .eq("userId", userId);
+    if (voice_messages.length === 0)
+        return { voices: null, currentVoice: null };
+    voice_messages = voice_messages[0];
+    voice_messages.voices = JSON.parse(voice_messages.voices);
+    return voice_messages;
+}
+
+async function update(userId, voices, currentVoice) {
+    voices = JSON.stringify(voices);
+    const { data, error } = await supabase
+        .from("voice_messages")
+        .update({ voices, currentVoice })
+        .eq("userId", userId)
+        .select();
+    return error;
+}
+async function updateCurrentVoice(userId, currentVoice) {
+    const { data, error } = await supabase
+        .from("voice_messages")
+        .update({ currentVoice })
+        .eq("userId", userId)
+        .select();
+    return error;
+}
+
+module.exports = {
+    insert,
+    updateCurrentVoice,
+    read,
+    update,
+};
